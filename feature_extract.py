3bcb0462ce79fdc522c4fdd77b37818832387474
James Jin <jamesjin0516@gmail.com>
2024-08-13 20:32:35 -0400

    Save & load model epoch and score; expose encodings & parameters

--- feature_extract.py
+++ feature_extract.py
@@ -14,10 +14,20 @@ class VLADDinoV2FeatureExtractor:
         if content["ckpt_path"] == "None":
             weights_source = content["model_type"]
             print(f"AnyLoc extractor ignores pipeline option (={pipeline}) because ckpt_path is None")
+            self.saved_state = {"epoch": 0, "best_score": 0}
         else:
             weights_source = os.path.join(content["ckpt_path"], "model_best.pth") if pipeline else content["ckpt_path"]
+            self.saved_state = torch.load(weights_source)
+            if self.saved_state.keys() == {"epoch", "best_score", "state_dict"}:
+                weights_source = os.path.join(content["ckpt_path"], "dino_model_temp.pth")
+                torch.save(self.saved_state["state_dict"], weights_source)
+            else:
+                self.saved_state = {"epoch": 0, "best_score": 0}
         # DinoV2 & VLAD settings
         self.dinov2_extractor = DinoV2ExtractFeatures(weights_source, content["desc_layer"], content["desc_facet"], device=self.device)
+        if os.path.exists(os.path.join(content["ckpt_path"], "dino_model_temp.pth")): os.remove(os.path.join(content["ckpt_path"], "dino_model_temp.pth"))
+        self.saved_state["state_dict"] = self.dinov2_extractor.dino_model.state_dict()
+
         vocab_dir = os.path.join(content["model_type"], f"l{content['desc_layer']}_{content['desc_facet']}_c{content['num_clusters']}")
         c_centers_file = os.path.join(root, content["cache_dir"], "vocabulary", vocab_dir, content["domain"], "c_centers.pt")
         c_centers = torch.load(c_centers_file)
@@ -26,15 +36,14 @@ class VLADDinoV2FeatureExtractor:
         self.vlad.fit(None)
 
     def __call__(self, images):
-        with torch.no_grad():
-            scaled_imgs = self.downscale(images)
-            b, c, h, w = scaled_imgs.shape
-            h_new, w_new = (h // 14) * 14, (w // 14) * 14
-            scaled_imgs = transforms.CenterCrop((h_new, w_new))(scaled_imgs)
-            # Extract descriptor
-            features = self.dinov2_extractor(scaled_imgs) # [num_imgs, num_patches, desc_dim]
-            vlad_feats = [self.vlad.generate(features[i].cpu().squeeze()) for i in range(features.shape[0])] # VLAD:  [agg_dim]
-            return features.detach().cpu(), torch.stack(vlad_feats).detach() # shape: [num_imgs, agg_dim]
+        scaled_imgs = self.downscale(images)
+        b, c, h, w = scaled_imgs.shape
+        h_new, w_new = (h // 14) * 14, (w // 14) * 14
+        scaled_imgs = transforms.CenterCrop((h_new, w_new))(scaled_imgs)
+        # Extract descriptor
+        features = self.dinov2_extractor(scaled_imgs) # [num_imgs, num_patches, desc_dim]
+        vlad_feats = [self.vlad.generate(features[i].cpu().squeeze()) for i in range(features.shape[0])] # VLAD:  [agg_dim]
+        return features, torch.stack(vlad_feats).to(self.device) # shape: [num_imgs, agg_dim]
     
     def downscale(self, images):
         if max(images.shape[-2:]) > self.max_image_size:
@@ -48,6 +57,27 @@ class VLADDinoV2FeatureExtractor:
                 w = self.max_image_size
             return functional.resize(images, (h, w), interpolation=functional.InterpolationMode.BICUBIC)
         return images
+
+    def set_train(self, is_train):
+        self.dinov2_extractor.dino_model.train(is_train)
+    
+    def torch_compile(self, float32=False, **compile_args):
+        self.dinov2_extractor.dino_model = torch.compile(self.dinov2_extractor.dino_model, **compile_args)
+        if float32:
+            self.dinov2_extractor.dino_model.to(torch.float32)
+    
+    def save_state(self, save_path, new_state):
+        new_state["state_dict"] = self.dinov2_extractor.dino_model.state_dict()
+        torch.save(new_state, save_path)
+
+    @property
+    def last_epoch(self): return self.saved_state["epoch"]
+
+    @property
+    def best_score(self): return self.saved_state["best_score"]
+
+    @property
+    def parameters(self): return self.dinov2_extractor.dino_model.parameters()
 
     @property
     def feature_length(self): return self.vlad.desc_dim * self.vlad.num_clusters
